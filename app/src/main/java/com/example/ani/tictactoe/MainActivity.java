15929b214b64faa719ce16e0c8f322cf83d02684
Anirudh Manoj <amanoj2@illinois.edu>
2017-06-16 00:37:28 -0500

    Add a menu for 1 and 2 player modes, change MainActivity to TwoPlayerActivity and make OnePlayerActicity the same as two player for now

--- app/src/main/java/com/example/ani/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/ani/tictactoe/MainActivity.java
@@ -1,205 +1,31 @@
 package com.example.ani.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int moves;
-    int[][] state;
-    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
-
-    TextView playerText;
-    LinearLayout winLayout;
-
-    private void updateTurnText() {
-        playerText.setText("Player " + ((moves % 2) + 1) + "'s Turn");
+    public void twoPlayer(View view) {
+        Intent intent = new Intent(getApplicationContext(), TwoPlayerActivity.class);
+        startActivity(intent);
     }
 
-    private void updateResultText(int n) {
-        TextView text = (TextView) findViewById(R.id.winnerText);
-        switch(n) {
-            case 1:
-            case 2:
-                text.setText("Player " + n + " won!");
-                break;
-            case 3:
-                text.setText("Draw!");
-                break;
-            default:
-                break;
-        }
-    }
-
-    private int checkForWinner() {
-        for (int i = 0; i < winningPositions.length; i++) {
-            int x1 = getX(winningPositions[i][0]), x2 = getX(winningPositions[i][1]), x3 = getX(winningPositions[i][2]);
-            int y1 = getY(winningPositions[i][0]), y2 = getY(winningPositions[i][1]), y3 = getY(winningPositions[i][2]);
-            if ((state[x1][y1] == state[x2][y2]) &&
-                    (state[x2][y2] == state[x3][y3]) &&
-                    (state[x3][y3] == state[x1][y1]) &&
-                    (state[x1][y1] != -1)) {
-                updateResultText(state[x1][y1] + 1);
-                endGame();
-                return state[x1][y1];
-            }
-        }
-        if(moves == 9) {
-            updateResultText(3);
-            endGame();
-            return 2;
-        }
-        return -1;
-    }
-
-    private int getX(int pos) {
-        int x = 0;
-        switch(pos) {
-            case 3:
-            case 4:
-            case 5:
-                x = 1;
-                break;
-            case 6:
-            case 7:
-            case 8:
-                x = 2;
-                break;
-            default:
-                break;
-        }
-        return x;
-    }
-
-    private int getY(int pos) {
-        int y = 0;
-        switch(pos) {
-            case 1:
-            case 4:
-            case 7:
-                y = 1;
-                break;
-            case 2:
-            case 5:
-            case 8:
-                y = 2;
-                break;
-            default:
-                break;
-        }
-        return y;
-    }
-
-    private void resetState() {
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                state[i][j] = -1;
-            }
-        }
-    }
-
-    public void dropIn(View view) {
-        if(moves == 0) {
-            winLayout.setVisibility(View.INVISIBLE);
-        }
-
-        ImageView counter = (ImageView) view;
-        int pos = Integer.parseInt(counter.getTag().toString());
-        int x = getX(pos), y = getY(pos);
-        if(state[x][y] != -1) {
-            Toast.makeText(this, "You can't replace a piece!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        counter.setImageResource(0);
-        counter.setTranslationY(-1000f);
-        if((moves % 2) == 0) {
-            counter.setImageResource(R.drawable.x);
-        }
-        else {
-            counter.setImageResource(R.drawable.o);
-        }
-        counter.animate().translationYBy(1000f).alpha(1f).rotation(360f).setDuration(300);
-
-        state[x][y] = (moves % 2);
-        moves++;
-        if(checkForWinner() != -1) {
-            return;
-        }
-        updateTurnText();
-
-    }
-
-    public void endGame() {
-        playerText.setVisibility(View.INVISIBLE);
-
-        android.support.v7.widget.GridLayout grid = (android.support.v7.widget.GridLayout) findViewById(R.id.gameBoard);
-        for(int i = 0; i < grid.getChildCount(); i++) {
-            grid.getChildAt(i).setClickable(false);
-        }
-        grid.setClickable(false);
-
-        winLayout.setVisibility(View.VISIBLE);
-        winLayout.animate().alpha(1f).setDuration(300);
-    }
-
-    public void restart(View view) {
-        moves = 0;
-        resetState();
-
-        android.support.v7.widget.GridLayout grid = (android.support.v7.widget.GridLayout) findViewById(R.id.gameBoard);
-        for(int i = 0; i < grid.getChildCount(); i++) {
-            ImageView child = (ImageView) grid.getChildAt(i);
-            child.setClickable(true);
-            child.animate().alpha(0f).setDuration(300);
-            child.setRotation(-360f);
-        }
-        grid.setClickable(true);
-
-        winLayout.animate().alpha(0f).setDuration(300);
-
-        playerText.setVisibility(View.VISIBLE);
-        updateTurnText();
+    public void onePlayer(View view) {
+        Intent intent = new Intent(getApplicationContext(), OnePlayerActivity.class);
+        startActivity(intent);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        state = new int[3][3];
-        resetState();
-
-        playerText = (TextView) findViewById(R.id.playerView);
-        winLayout = (LinearLayout) findViewById(R.id.winnerLayout);
-
-        moves = 0;
-        updateTurnText();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
-            case R.id.restart:
-                restart(null);
-                break;
-            default:
-                break;
-        }
-        return super.onOptionsItemSelected(item);
+    public void onBackPressed() {
+        super.onBackPressed();
+        moveTaskToBack(true);
     }
 }
