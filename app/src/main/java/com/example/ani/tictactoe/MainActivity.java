8a4b8d7e2f20d36c669c66c92fe2d53bc954a5b4
Anirudh Manoj <amanoj2@illinois.edu>
2017-06-15 23:06:32 -0500

    Rework winning check in 2D form instead of 1D array

--- app/src/main/java/com/example/ani/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/ani/tictactoe/MainActivity.java
@@ -6,7 +6,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -15,7 +14,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int player;
-    int[] state = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
+    int[][] state;
     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
     TextView playerText;
@@ -46,16 +45,64 @@ public class MainActivity extends AppCompatActivity {
 
     private int checkForWinner() {
         for(int i = 0; i < winningPositions.length; i++) {
-            if((state[winningPositions[i][0]] == state[winningPositions[i][1]]) &&
-                    (state[winningPositions[i][1]] == state[winningPositions[i][2]]) &&
-                    (state[winningPositions[i][0]] == state[winningPositions[i][2]]) &&
-                    (state[winningPositions[i][0]] != -1)) {
-                updateResultText(state[winningPositions[i][0]] + 1);
+            int x1 = getX(winningPositions[i][0]), x2 = getX(winningPositions[i][1]), x3 = getX(winningPositions[i][2]);
+            int y1 = getY(winningPositions[i][0]), y2 = getY(winningPositions[i][1]), y3 = getY(winningPositions[i][2]);
+            if((state[x1][y1] == state[x2][y2]) &&
+                    (state[x2][y2] == state[x3][y3]) &&
+                    (state[x3][y3] == state[x1][y1]) &&
+                    (state[x1][y1] != -1)) {
+                updateResultText(state[x1][y1] + 1);
                 endGame();
                 return 0;
             }
         }
         return -1;
+    }
+
+    private int getX(int pos) {
+        int x = 0;
+        switch(pos) {
+            case 3:
+            case 4:
+            case 5:
+                x = 1;
+                break;
+            case 6:
+            case 7:
+            case 8:
+                x = 2;
+                break;
+            default:
+                break;
+        }
+        return x;
+    }
+
+    private int getY(int pos) {
+        int y = 0;
+        switch(pos) {
+            case 1:
+            case 4:
+            case 7:
+                y = 1;
+                break;
+            case 2:
+            case 5:
+            case 8:
+                y = 2;
+                break;
+            default:
+                break;
+        }
+        return y;
+    }
+
+    private void resetState() {
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                state[i][j] = -1;
+            }
+        }
     }
 
     public void dropIn(View view) {
@@ -64,7 +111,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         ImageView counter = (ImageView) view;
-        if(state[Integer.parseInt(counter.getTag().toString())] != -1) {
+        int pos = Integer.parseInt(counter.getTag().toString());
+        int x = getX(pos), y = getY(pos);
+        if(state[x][y] != -1) {
             Toast.makeText(this, "You can't replace a piece!", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -78,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
         }
         counter.animate().translationYBy(1000f).alpha(1f).rotation(360f).setDuration(300);
 
-        state[Integer.parseInt(counter.getTag().toString())] = (player % 2);
+        state[x][y] = (player % 2);
         if(checkForWinner() == 0) {
             return;
         }
@@ -108,8 +157,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void restart(View view) {
         player = 0;
-        for(int i = 0; i < state.length; i++)
-            state[i] = -1;
+        resetState();
 
         android.support.v7.widget.GridLayout grid = (android.support.v7.widget.GridLayout) findViewById(R.id.gameBoard);
         for(int i = 0; i < grid.getChildCount(); i++) {
@@ -130,6 +178,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        state = new int[3][3];
+        resetState();
 
         playerText = (TextView) findViewById(R.id.playerView);
         winLayout = (LinearLayout) findViewById(R.id.winnerLayout);
